d87c6bca7216f27af84f6f02d3ef2881c6047c81
sahil0233 <73436999+sahil0233@users.noreply.github.com>
2024-01-20 13:38:34 +0530

    contact background adjustment

--- pages/components/ContactMe.js
+++ pages/components/ContactMe.js
@@ -8,7 +8,7 @@ const ContactMe = () => {
     </div>
     <div className="container px-6 md:px-12">
       <div
-        className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
+        className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,1%,.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
         <div className="mb-12 grid gap-x-6 md:grid-cols-1 lg:grid-cols-3">
           <div className="mx-auto mb-12 text-center lg:mb-0">
             <a href='mailto:sahil.gangwani2024@gmail.com'><img className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400" src ="/mail.svg" /></a>
