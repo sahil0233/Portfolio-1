077063f025469cb8fd5e2df94381d86211937d51
sahil0233 <73436999+sahil0233@users.noreply.github.com>
2024-01-20 13:46:23 +0530

    contact background adjustment-2

--- pages/components/ContactMe.js
+++ pages/components/ContactMe.js
@@ -8,20 +8,20 @@ const ContactMe = () => {
     </div>
     <div className="container px-6 md:px-12">
       <div
-        className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,1%,.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
+        className="block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,1%,.4)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
         <div className="mb-12 grid gap-x-6 md:grid-cols-1 lg:grid-cols-3">
           <div className="mx-auto mb-12 text-center lg:mb-0">
             <a href='mailto:sahil.gangwani2024@gmail.com'><img className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400" src ="/mail.svg" /></a>
             <h6 className="font-medium text-white text-md"> <a href="mailto:sahil.gangwani2024@gmail.com">sahil.gangwani2024@gmail.com</a></h6>
           </div>
-          <div className="mx-auto mb-12 text-center lg:mb-0">
+          <div className="mx-auto mb-12 text-black lg:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
               stroke="currentColor" className="mx-auto mb-6 h-8 w-8 text-primary dark:text-primary-400">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
               <path strokeLinecap="round" strokeLinejoin="round"
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
             </svg>
-            <h6 className="font-medium text-white text-md">Jaipur, Rajasthan, India</h6>
+            <h6 className="font-medium text-black text-md">Jaipur, Rajasthan, India</h6>
           </div>
           <div className="mx-auto mb-6 text-center md:mb-0">
             <a href='tel:+918239279999'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
@@ -29,7 +29,7 @@ const ContactMe = () => {
               <path strokeLinecap="round" strokeLinejoin="round"
                 d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
             </svg></a>
-            <h6 className="font-medium text-white text-md"><a href="tel:+918239279999">+91 8239279999</a></h6>
+            <h6 className="font-medium text-black text-md"><a href="tel:+918239279999">+91 8239279999</a></h6>
           </div>
         </div>
       </div>
